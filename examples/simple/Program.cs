444a43a7fa002720da1fdfe272ce3ef88a1c395e
NikiforovAll <nikiforovalekcey@gmail.com>
2020-04-28 22:49:31 +0300

    docs: add EventSourceListener to simple example

--- examples/simple/Program.cs
+++ examples/simple/Program.cs
@@ -5,12 +5,16 @@ using XMindAPI.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
+using System.Diagnostics.Tracing;
+using System.Text;
 
 namespace simple
 {
     class Program
     {
-        private static ILogger<Program> logger;
+        public static ILogger<Program> Logger { get; private set;}
+
+        public static TextWriter Out { get; set; } = Console.Out;
 
         static async Task Main(
             string demo = "memory"
@@ -20,10 +24,10 @@ namespace simple
                 .AddLogging(configure => configure.AddConsole())
                 .BuildServiceProvider();
 
-            logger = serviceProvider.GetService<ILoggerFactory>()
+            Logger = serviceProvider.GetService<ILoggerFactory>()
                 .CreateLogger<Program>();
 
-
+            using var eventSourceListener = new EventSourceListener("XMind-XMindCsharpEventSource");
             switch (demo)
             {
                 case "file":
@@ -47,11 +51,49 @@ namespace simple
         {
             string basePath = Path.Combine(Path.GetTempPath(), "xmind-test");
             var bookName = "test.xmind";
-            logger.LogInformation(default(EventId), $"Base path: ${Path.Combine(basePath, bookName)}");
+            Logger.LogInformation(default(EventId), $"Base path: ${Path.Combine(basePath, bookName)}");
             var book = new XMindConfiguration()
                 .WithFileWriter(basePath, true)
                 .CreateWorkBook(bookName);
             await book.Save();
+        }
+    }
+    sealed class EventSourceListener : EventListener
+    {
+        private readonly string _eventSourceName;
+        private readonly StringBuilder _messageBuilder = new StringBuilder();
+
+        public EventSourceListener(string name)
+        {
+            _eventSourceName = name;
+        }
+
+        protected override void OnEventSourceCreated(EventSource eventSource)
+        {
+            base.OnEventSourceCreated(eventSource);
+
+            if (eventSource.Name == _eventSourceName)
+            {
+                EnableEvents(eventSource, EventLevel.LogAlways, EventKeywords.All);
+            }
+        }
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            base.OnEventWritten(eventData);
+
+            string message;
+            lock (_messageBuilder)
+            {
+                _messageBuilder.Append("Event ");
+                _messageBuilder.Append(eventData.EventSource.Name);
+                _messageBuilder.Append(" - ");
+                _messageBuilder.Append(eventData.EventName);
+                _messageBuilder.Append(" : ");
+                _messageBuilder.AppendJoin(',', eventData.Payload);
+                message = _messageBuilder.ToString();
+                _messageBuilder.Clear();
+            }
+            Console.WriteLine(message);
         }
     }
 }
